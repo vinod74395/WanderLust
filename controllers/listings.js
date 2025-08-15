3b6ef35c9125ad8f1971399be9d993f0af65385f
vinod74395 <kvinod74395@gmail.com>
2025-08-15 22:52:33 +0530

    Fix Mapbox import case for Render

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,5 @@
 const Listing=require("../model/listing");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/Geocoding');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken= process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 module.exports.index= async (req, res) => {
